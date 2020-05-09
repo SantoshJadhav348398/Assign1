b404d31454abe3e59a7566f7db1704bf2e793787
Santosh <axelblaze348398@gmail.com>
2020-05-09 19:34:46 +0530

    commit for mutex

---
+++ MultiThreadingA5/MultiThreadingA5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadingA5
+{
+    class Program
+    {
+        static Mutex mut = new Mutex();
+        static void Main(string[] args)
+        {
+            Thread ping = new Thread(Monitored);
+            Thread pong = new Thread(Monitored);
+            ping.Name = "ping";
+            pong.Name = "pong";
+            ping.Start();
+            pong.Start();
+            Thread.Sleep(4000);
+            Console.WriteLine("Time to end");
+        }
+
+        static void Monitored()
+        {
+
+            mut.WaitOne();
+            Console.WriteLine($"Current Thread {Thread.CurrentThread.Name} started");
+            Thread.Sleep(1000);
+            Console.WriteLine($"Current Thread {Thread.CurrentThread.Name} stopped");
+
+            mut.ReleaseMutex();
+
+        }
+    }
+}
