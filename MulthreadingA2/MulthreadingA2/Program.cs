e89235750b60be3f7b2ddfbfb6c5c6eab28fc2d8
Santosh <axelblaze348398@gmail.com>
2020-05-09 15:38:27 +0530

    Second AssignMent

---
+++ MulthreadingA2/MulthreadingA2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+namespace MulthreadingA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread eena = new Thread(StartTheThreading);
+            Thread meena = new Thread(StartTheThreading);
+            Thread deeka = new Thread(StartTheThreading);
+            eena.Start();
+            meena.Start();
+            deeka.Start();
+            Console.WriteLine("Threads are started");
+        }
+
+        static void StartTheThreading()
+        {
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine("Local = {0}", i);
+        }
+    }
+}
