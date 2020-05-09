274afb5f04736ef05cc0e9aea2c7e06d9ccc8d11
Santosh <axelblaze348398@gmail.com>
2020-05-09 15:34:58 +0530

    first assignment

---
+++ MultiThreadindA1/MultiThreadindA1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadindA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(StartTheThreading);
+            t1.Start();
+            Thread.Sleep(4000);
+            Console.WriteLine("Started the Thread");
+        }
+
+        static void StartTheThreading()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("local = {0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
