6a941aeac543fa1d7e1a625df7428bcb4c90a35f
Santosh <axelblaze348398@gmail.com>
2020-05-10 20:39:22 +0530

    commit for A15

---
+++ MultiThreadingA15/MultiThreadingA15/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MultiThreadingA15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            System.Threading.Tasks.Task.Run(() => { Console.WriteLine("Performing the tasks"); });
+        }
+    }
+}
